32a7206a74815f253358635d14b096f9612b88a5
Sergey Pashnin <sspinit88@gmail.com>
2020-10-16 16:22:57 +0300

    Add hasky.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnIinit {
   title = 'angular-custom-components-with-tests';
+
 }
